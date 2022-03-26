85c32f283b6901bbdbdb4916096e18b12ca74f16
THE-REPENT-STAR <3266306682@qq.com>
2022-03-26 22:20:40 +0800

    V-0.0.2

--- Deleter.py
+++ Deleter.py
@@ -1,5 +1,6 @@
 # -*- coding:utf8 -*-
 import os
+#import send2trash
 
 def del_files(path, end_name):
     for root, dirs, files in os.walk(path): #不要删这个看似没用的dirs
@@ -8,19 +9,37 @@ def del_files(path, end_name):
                 os.remove(os.path.join(root, name))
                 print("已删除文件: " + os.path.join(root, name)) #报告已删除的文件
 
+def continue_or_not():
+    go_on = input("请问还要继续嘛？输入n取消")
+    if go_on == 'n':
+        return False
 
 if __name__ == "__main__":
-    path = input("请粘贴要进行操作的文件夹路径：")
-    #print(path)
-    end_name = input("请输入要删除文件的文件名后方字符串共同点：")
-    determination = input("确定？文件不会放入回收站！确认请输入y,输入n取消：")
-    if determination == "y":
-        del_files(path, end_name)
-        print("ok")
-    elif determination == "n":
-        print("已取消")
-    else:
-        print("输入错误")
+    while (True):
+        try:
+            path = input("请粘贴要进行操作的文件夹路径：")
+            #print(path)
+            end_name = input("请输入要删除文件的文件名后方字符串共同点：")
+            determination = input("确定？文件不会放入回收站！确认请输入y,输入n取消：")
+            if determination == "y":
+                del_files(path, end_name)
+                print("ok")
+                if continue_or_not() is False:
+                    break
+                else:
+                    continue
+            elif determination == "n":
+                print("已取消")
+                if continue_or_not() is False:
+                    break
+                else:
+                    continue
+            else:
+                print("输入错误")
+        except:
+            print("程序出错！！")
+    
+    print('Bye!')
     os.system("pause") #没任何用处的暂停
 
 # code by RepentStar
